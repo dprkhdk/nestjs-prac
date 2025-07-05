1be32b4a6f1dd921fde3458e0dfd5e66f40d0ed3
Denys Prykhodko <denys.prykh@gmail.com>
2025-07-05 22:02:01 +0300

    my first step in nest js

---
+++ src/app.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { FlowersModule } from './flowers/flowers.module';
+
+
+@Module({
+  imports: [FlowersModule],
+
+})
+export class AppModule {}
---
+++ src/flowers/flowers.controller.ts
@@ -0,0 +1,14 @@
+import {Controller, Get, UseGuards} from '@nestjs/common';
+import { FlowersService } from './flowers.service';
+import {AuthGuard} from "../conception/guard";
+
+@Controller('flowers')
+export class FlowersController {
+  constructor(private readonly flowersService: FlowersService) {}
+
+  @Get()
+  @UseGuards(AuthGuard)
+  findAll(){
+    return this.flowersService.findAll()
+  }
+}
